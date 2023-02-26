2499d979715b0fd157ad736128c4b24da9cde433
abh1r4m <2010030523@klh.edu.in>
2023-02-26 15:11:01 +0530

    Update and Insert added

--- backend/lib/courseLib.js
+++ backend/lib/courseLib.js
@@ -4,6 +4,7 @@ module.exports.getAllCourses = async function(){ //Similar to select *
 return await courseModel.find({});
 //{}-> it is like select *, and we can put constraints inside to get 
 }//await is used when there are resources that are not in scope of code such as database data
+
 module.exports.createFirstCourse = async function(){ //Similar to insert 
 const courses = await courseModel.find({});
 if(courses && courses.length==0){
@@ -11,4 +12,12 @@ const firstCourse = {"title": "BZ DSA","level":"Easy"};
 const course = new courseModel(firstCourse);
 await course.save();
 }
+}
+
+module.exports.createCourse = async function(courseInput){ //Similar to insert 
+    const course = new courseModel(courseInput);
+    await course.save();
+}
+module.exports.updateCourse = async function(courseId,updatedInput){ //Similar to insert 
+    const course = await courseModel.findOneAndUpdate({_id : courseId},updatedInput);
 }
