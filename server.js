b0a3dbad52f1352cfd272226d0b00e09b0349220
abh1r4m <2010030523@klh.edu.in>
2023-02-26 11:00:24 +0530

    Removing unnecessary points and adding nodemon,serving html file

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const express = require("express");//creating application
 
 const app = express();//calling
 var name = "Abhiram"
-
+//use nodemon server.js to run because it will dynamically update with changes
 app.use(express.static("frontend"))
 
 app.get("/",function(req,res){
-    res.sendFile("Hello World!")
-});
-app.get("/index",function(req,res){
     res.sendFile(__dirname+"frontend/index.html")
-});
-
-app.get("/myname",function(req,res){
-    res.send(`My name is ${name}`)
 });
 
 app.listen(3000,function(){
