1bd9fa4ae2eb4e87882b290e8dcf406f4014bb93
abh1r4m <2010030523@klh.edu.in>
2023-02-26 15:40:47 +0530

    installedd bootstrap

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,13 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-// import './App.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button  from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    <h1>Hello</h1>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Neetcode Clone</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/courses">Courses</Nav.Link>
+            <Nav.Link href="/practice">Practice</Nav.Link>
+            <Nav.Link href="/roadmap">Roadmap</Nav.Link>
+            <Nav.Link href="/pro">Pro</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div>
+        <Button variant="outline-primary">
+          Dark Mode
+        </Button>
+        <Button variant="primary">
+          Sign In
+        </Button>
+      </div>
     </div>
   )
 }
