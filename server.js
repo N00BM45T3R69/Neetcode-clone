670f819d9d3b19d1e04f2ca60738e730d44a67e4
abh1r4m <2010030523@klh.edu.in>
2023-02-26 14:14:56 +0530

    Adding Database to project

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const mongoose = require("mongoose")
+
+const dotenv = require("dotenv")
+dotenv.config()
+const connstr = process.env.MONGO_CONN_STRING;
 const express = require("express");//creating application
 
+
 const app = express();//calling
+mongoose.set('strictQuery', true);
+
+mongoose.connect(connstr,function(err){ //connect to database
+    if(err)
+    console.err; // or console.log(err);
+    else{
+        console.log("DB Connection Successful");
+        // We put this here because web server should only start after connection with database
+        app.listen(3000,function(){
+        console.log("Server running on http://localhost:3000")
+        });
+}
+});
+
 var name = "Abhiram"
 //use nodemon server.js to run because it will dynamically update with changes
 app.use(express.static("frontend"))
@@ -9,6 +29,3 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"frontend/index.html")
 });
 
-app.listen(3000,function(){
-console.log("Server running on http://localhost:3000")
-});
