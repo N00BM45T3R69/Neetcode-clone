505aae8eafb2563ea3b0bb5ba82b6d17718d9698
abh1r4m <2010030523@klh.edu.in>
2023-02-26 16:18:30 +0530

    Counter

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import OurNavBar from './Components/Navbar'
-
+import Button from 'react-bootstrap/esm/Button'
 function App() {
-
+  const [num,setNum]= useState(0);
+  const counter = () =>{
+    setNum(num+1);
+  }
   return (
     <div className="App">
      <OurNavBar></OurNavBar>
+     
+     <Button onClick={counter}>Click here : {num}</Button>
     </div>
   )
 }
